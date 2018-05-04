40a6582e33e506ab7a91b3aa660794b0729639f6
eva <evasergeeva97@gmail.com>
2018-05-04 20:29:59 +0200

    initial commit

---
+++ app/src/main/java/com/example/eva/calculator/Main.java
@@ -0,0 +1,41 @@
+package com.example.eva.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onPlusButtonClick (View v) {
+        EditText el1 = (EditText)findViewById(R.id.Num1);
+        EditText el2 = (EditText)findViewById(R.id.Num2);
+        TextView resText = (TextView)findViewById(R.id.Result);
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 + num2;
+        resText.setText(String.format("%s",resSum));
+
+    }
+
+    public void onMinusButtonClick (View v) {
+        EditText el1 = (EditText)findViewById(R.id.Num1);
+        EditText el2 = (EditText)findViewById(R.id.Num2);
+        TextView resText = (TextView)findViewById(R.id.Result);
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resRas = num1 - num2;
+        resText.setText(String.format("%s",resRas));
+
+    }
+}
